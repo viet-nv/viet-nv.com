47ebc0446beb3d5f6295f742742a1cae14263a19
viet-nv <vietnv1304@gmail.com>
2020-07-22 01:15:32 +0700

    Fix menu button outline

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -41,8 +41,8 @@ const Header: React.FC = () => {
       />
       <div className={cx('sticky top-0 bg-white', { shadow: showShadow })}>
         <Container className="flex justify-between items-center h-16 md:h-20">
-          <div
-            className="block md:hidden cursor-pointer p-3"
+          <button
+            className="block md:hidden p-3 focus:outline-none"
             onClick={() => setShowSideDrawer((prev) => !prev)}
           >
             <svg
@@ -53,7 +53,7 @@ const Header: React.FC = () => {
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
-          </div>
+          </button>
 
           <Link href="/">
             <a>
@@ -75,7 +75,7 @@ const Header: React.FC = () => {
               })}
             </div>
 
-            <div className="p-3 cursor-pointer">
+            <button className="p-3">
               <svg
                 stroke="currentColor"
                 fill="currentColor"
@@ -87,7 +87,7 @@ const Header: React.FC = () => {
               >
                 <path d="M443.5 420.2L336.7 312.4c20.9-26.2 33.5-59.4 33.5-95.5 0-84.5-68.5-153-153.1-153S64 132.5 64 217s68.5 153 153.1 153c36.6 0 70.1-12.8 96.5-34.2l106.1 107.1c3.2 3.4 7.6 5.1 11.9 5.1 4.1 0 8.2-1.5 11.3-4.5 6.6-6.3 6.8-16.7.6-23.3zm-226.4-83.1c-32.1 0-62.3-12.5-85-35.2-22.7-22.7-35.2-52.9-35.2-84.9 0-32.1 12.5-62.3 35.2-84.9 22.7-22.7 52.9-35.2 85-35.2s62.3 12.5 85 35.2c22.7 22.7 35.2 52.9 35.2 84.9 0 32.1-12.5 62.3-35.2 84.9-22.7 22.7-52.9 35.2-85 35.2z"></path>
               </svg>
-            </div>
+            </button>
           </div>
         </Container>
       </div>
